6e087359f251a6225ea9030c2e3edfb4200428ed
Davin <davin.leonardo@binus.ac.id>
2020-07-29 09:09:18 +0700

    Design changes

---
+++ src/protect.js
@@ -0,0 +1,13 @@
+function reveal(){ 
+    var provider = "gmail";
+    var occupation = "composer";
+    var text = document.createElement("a");
+    var initial = "cgdl";
+    var button = document.getElementById("view");
+    button.className += " hide";
+    text.appendChild(document.createTextNode(initial + occupation + '@' + provider + ".com")); 
+    text.href = "mailto:" + initial + occupation + '@' + provider + ".com";
+    var email = document.getElementById("email");
+    email.appendChild(text)
+}
+    
